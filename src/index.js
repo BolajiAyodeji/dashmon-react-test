03cb6b2f80cafa0b7ba2ebda65945860e23badcb
bolajiayodeji <sirbeejay1@gmail.com>
2019-01-02 19:31:56 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Button from "./button.jsx";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Button type={"danger"}>Delete</Button>
+      <Button type={"primary"} size={"small"}>
+        Approve
+      </Button>
+      {/* Button Group lets user select only one value at a time */}
+      {/*
+      <Button.Group value={1}>
+        <Button value={0}>First Value</Button>
+        <Button value={1}>Second Value</Button>
+      </Button.Group>
+      */}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
